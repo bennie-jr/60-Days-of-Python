3d32b0d66653cbe0b49b2e1ade6a476a7749207c
Bennie Sowah Jr <bensowahjr@gmail.com>
2025-03-04 23:48:26 +0000

    Add files for day 56, food order app with django

---
+++ day56/restaurant_menu/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import Items
+
+class MenuList(generic.ListView):
+    queryset = Items.objects.order_by("date_created")
+    template_name = "index.html"
+
+
+    def get_context_data(self):
+        context = {"meals": ["Pizza", "Pasta"],
+                   "ingredients": ["things"]}
+        return context
+
+
+class MenuItemDetail(generic.DetailView):
+    model = Items
+    template_name = "menu_item_detail.html"
